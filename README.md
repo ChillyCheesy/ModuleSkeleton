068987c6506dfb0d3f8f1bf73acadb242e33fbbc
geoffrey-vaniscotte <vaniscotte.geoffrey@hotmail.com>
2023-01-12 15:14:32 +0100

    Init skeleton module

---
+++ README.md
@@ -0,0 +1,4 @@
+# Skeleton Module
+
+It is a Module for Modulo. 
+It is an empty module to quickly start working on a new module.
---
+++ src/main/java/com/chillycheesy/skeleton/SkeletonModule.java
@@ -0,0 +1,27 @@
+package com.chillycheesy.skeleton;
+
+import com.chillycheesy.modulo.modules.Module;
+
+/**
+ *
+ *
+ * @author chillycheesy
+ */
+public class SkeletonModule extends Module {
+
+    @Override
+    protected void onLoad() {
+
+    }
+
+    @Override
+    protected void onStart() {
+
+    }
+
+    @Override
+    protected void onStop() {
+
+    }
+
+}
